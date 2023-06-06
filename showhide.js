414d1eb4bd5e8b58470b363e8b9f4146860636bc
diogofalardo <work.diogofalardo@gmail.com>
2023-06-06 17:23:26 +0100

    Create showhide.js

---
+++ showhide.js
@@ -0,0 +1,10 @@
+var div = document.getElementById("div#1");
+
+function ShowHide(){
+    if(div.style.display == 'block'){
+        div.style.display = 'none';
+    }
+    else{
+        div.style.display = 'block';
+    }
+}
